e1996ef1d3e2b76cf1e5116d6b812402a1ecb876
Oleg Terentev <terentjev.dr@yandex.ru>
2023-03-28 22:10:17 +0300

    new repo

---
+++ src/main/java/ru/work/turborender/TurborenderApplication.java
@@ -0,0 +1,13 @@
+package ru.work.turborender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurborenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurborenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/work/turborender/TurborenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.work.turborender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurborenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
